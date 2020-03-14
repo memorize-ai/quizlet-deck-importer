be03c31951d21404a9039dbdec0ea173a55afd39
Ken Mueller <kenmueller0@gmail.com>
2020-03-13 19:38:19 -0700

    start

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import loadDecks from './load-decks'
 import importDecks from './import-decks'
 import { sleep } from './helpers'
 
-const main = async (): Promise<void> => {
+const main = async (load: string = 'true'): Promise<void> => {
 	try {
-		await loadDecks()
+		if (load === 'true')
+			await loadDecks()
+		
 		await importDecks()
 		
+		console.log('Sleeping for one hour...')
 		await sleep(1000 * 60 * 60)
 	} catch (error) {
 		console.error(error)
@@ -18,4 +21,4 @@ const main = async (): Promise<void> => {
 export default main
 
 if (require.main === module)
-	main()
+	main(process.argv.length > 3 ? process.argv[2] : undefined)
